01cc10f199be582cd3fc8088ac93b28a353c5a29
N1ghtsky0 <20korea01@gmail.com>
2025-04-07 18:08:34 +0900

    feat: 기본 시큐리티 설정

---
+++ src/main/java/xyz/jiwook/board/global/seucirtyConfig/SecurityConfig.java
@@ -0,0 +1,25 @@
+package xyz.jiwook.board.global.seucirtyConfig;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .oauth2Login(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().permitAll())
+        ;
+        return http.build();
+    }
+}
