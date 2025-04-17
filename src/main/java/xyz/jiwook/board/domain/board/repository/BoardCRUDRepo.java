f3c6113f13d2227d47ec3fbc5eed0a5964dfaf0e
N1ghtsky0 <20korea01@gmail.com>
2025-04-17 16:42:05 +0900

    reset

--- src/main/java/xyz/jiwook/board/domain/board/repository/BoardCRUDRepo.java
+++
@@ -1,7 +0,0 @@
-package xyz.jiwook.board.domain.board.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import xyz.jiwook.board.domain.board.model.BoardEntity;
-
-public interface BoardCRUDRepo extends CrudRepository<BoardEntity, Long> {
-}
