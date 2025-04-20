e3e7a9dbe71f3c77a112caafadebf2dd0339a144
N1ghtsky0 <20korea01@gmail.com>
2025-04-20 15:25:43 +0900

    feat: UserDetailsImpl, 로그인 유저 어노테이션

---
+++ src/main/java/xyz/jiwook/board/util/UserDetailsImpl.java
@@ -0,0 +1,26 @@
+package xyz.jiwook.board.util;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import xyz.jiwook.board.entity.MemberEntity;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public record UserDetailsImpl(MemberEntity memberEntity) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return memberEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return memberEntity.getUsername();
+    }
+}
--- src/main/java/xyz/jiwook/board/util/UserDetailsServiceImpl.java
+++ src/main/java/xyz/jiwook/board/util/UserDetailsServiceImpl.java
@@ -1,16 +1,12 @@
 package xyz.jiwook.board.util;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import xyz.jiwook.board.dao.MemberRepo;
 import xyz.jiwook.board.entity.MemberEntity;
-
-import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +16,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         MemberEntity memberEntity = memberRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
-        return new User(memberEntity.getUsername(), "", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+        return new UserDetailsImpl(memberEntity);
     }
 }
