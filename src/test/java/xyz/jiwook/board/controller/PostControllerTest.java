64d623ef5537f723405f6c379796c55f9fcd0361
N1ghtsky0 <20korea01@gmail.com>
2025-04-21 13:31:53 +0900

    test: 게시글 수정

--- src/test/java/xyz/jiwook/board/controller/PostControllerTest.java
+++ src/test/java/xyz/jiwook/board/controller/PostControllerTest.java
@@ -3,10 +3,12 @@ package xyz.jiwook.board.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.ResultActions;
+import xyz.jiwook.board.service.PostService;
 import xyz.jiwook.board.vo.PostVO;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +18,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 class PostControllerTest extends CommonTestController {
+    @Autowired
+    private PostService postService;
+
     @Test
     @DisplayName("게시글 작성")
     void createPost() throws Exception {
@@ -68,17 +73,22 @@ class PostControllerTest extends CommonTestController {
     @DisplayName("게시글 업데이트")
     void updatePost() throws Exception {
         //given
-        final String URI = "/posts/1";
         PostVO postVO = new PostVO();
+        postVO.setAuthor(TEST_USERNAME);
         postVO.setTitle("mock title");
         postVO.setContent("mock content");
-        final String ACCESS_TOKEN = "test token";
+        long postSeq = postService.createPost(postVO);
+
+        final String URI = "/posts/" + postSeq;
+        PostVO updatedPostVO = new PostVO();
+        updatedPostVO.setTitle("updated mock title");
+        updatedPostVO.setContent("updated mock content");
 
         //when
         ResultActions result = mvc.perform(post(URI)
-                        .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                        .header("Authorization", "Bearer " + USABLE_ACCESS_TOKEN)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(postVO)))
+                        .content(objectMapper.writeValueAsString(updatedPostVO)))
                 .andDo(print());
 
         //then
