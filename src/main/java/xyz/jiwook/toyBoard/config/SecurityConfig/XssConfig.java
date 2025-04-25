cdd1327120bc55be5a8e9b2f4968ae761f3751b0
N1ghtsky0 <20korea01@gmail.com>
2025-04-25 15:01:20 +0900

    feat: XSS filter

---
+++ src/main/java/xyz/jiwook/toyBoard/config/SecurityConfig/XssConfig.java
@@ -0,0 +1,23 @@
+package xyz.jiwook.toyBoard.config.SecurityConfig;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class XssConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+        return objectMapper;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+}
