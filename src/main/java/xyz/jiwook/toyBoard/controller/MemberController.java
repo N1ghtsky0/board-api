af56183dc01fed2a5c11b3a30aacbce77d003beb
N1ghtsky0 <20korea01@gmail.com>
2025-04-25 17:52:24 +0900

    feat: 내가 작성한 게시글 목록

---
+++ src/main/java/xyz/jiwook/toyBoard/controller/MemberController.java
@@ -0,0 +1,28 @@
+package xyz.jiwook.toyBoard.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.jiwook.toyBoard.config.SecurityConfig.SecurityUtil;
+import xyz.jiwook.toyBoard.service.PostService;
+import xyz.jiwook.toyBoard.vo.reponse.PostSummaryVO;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+    private final PostService postService;
+    private final SecurityUtil securityUtil;
+    @RequestMapping("/me/posts")
+    public ResponseEntity<Page<PostSummaryVO>> getMyPostList(
+            @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+        final String searchType = "A";
+        final String searchKeyword = securityUtil.getCurrentUsername();
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return ResponseEntity.ok(postService.searchPostList(pageRequest, searchType, searchKeyword));
+    }
+}
